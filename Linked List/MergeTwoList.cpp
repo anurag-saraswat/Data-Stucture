af7caef492ccb1e841abeecb0e38ad1d0fad7eff
anurag-saraswat <saraswatanurag14@gmail.com>
2020-04-30 05:51:36 +0530

    Merge two list added

---
+++ Linked List/MergeTwoList.cpp
@@ -0,0 +1,107 @@
+#include "iostream"
+using namespace std;
+
+struct Node {
+	int key;
+	struct Node *next;
+}*first = NULL, *second = NULL, *third = NULL;
+
+
+void Display(struct Node *head) {
+
+	if (head == NULL) {
+		cout << "list is empty" << endl;
+		return;
+	}
+
+	cout << "Elemnts of list are: ";
+
+	struct Node * temp = head;
+
+	while (temp != NULL) {
+		cout << temp->key << "->";
+		temp = temp->next;
+	}
+
+	cout << "NULL" << endl;
+}
+
+struct Node* Create(int *arr , int size) {
+	struct Node *ptr = (struct Node*)malloc(sizeof(struct Node));
+	ptr->key = arr[0];
+	ptr->next = NULL;
+	struct Node *head = ptr;
+
+	for (int i = 1; i < size; i++) {
+
+		struct Node *temp = (struct Node*)malloc(sizeof(struct Node));
+		temp->key = arr[i];
+		temp->next = NULL;
+
+		ptr->next = temp;
+		ptr = ptr->next;
+	}
+
+	return head ;
+}
+
+void Merge() {
+
+	struct Node *iter = NULL;
+
+	if (first->key > second->key) {
+		third = second;
+		second = second->next;
+		third->next = NULL;
+	}
+	else {
+		third = first;
+		first = first->next;
+		third->next = NULL;
+	}
+
+
+	iter = third;
+	while (first and second) {
+
+		if (first->key > second->key) {
+			iter->next = second;
+			iter = iter->next;
+			second = second->next;
+			iter->next = NULL;
+		}
+		else {
+			iter->next = first;
+			iter = iter->next;
+			first = first->next;
+			iter->next = NULL;
+		}
+
+	}
+
+	if (first) {
+		iter->next = first;
+	}
+
+	if (second) {
+		iter->next = second;
+	}
+}
+
+int main() {
+
+	int size = 10;
+	int arr1[] = {1, 2, 3, 10, 20, 30, 45, 50, 70, 78};
+	first = Create(arr1, size);
+	Display(first);
+
+	size = 10;
+	int arr2[] = {10, 15, 25, 30, 42, 55, 66, 68, 82, 90};
+	second = Create(arr2, size);
+	Display(second);
+	Merge();
+	Display(third);
+
+
+	return 0;
+}
