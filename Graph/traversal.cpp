8e82f8539ca7d50b71d8bb8f4ed45cfecc3d41cc
anurag-saraswat <saraswatanurag14@gmail.com>
2020-05-28 08:37:16 +0530

    Depth First Traversal Added

--- Graph/traversal.cpp
+++ Graph/traversal.cpp
@@ -1,11 +1,12 @@
 #include "iostream"
 #include "Queue.h"
+#include "Stack.h"
 using namespace std;
 
 
 void BFS(int G[][7], int size , int node ) {
 
-	cout << "Breadth First Traversal of Graph is: ";
+	cout << "Breadth First Traversal of Graph " << "using source " << node << " is: ";
 
 	int visited[size] = {0};
 
@@ -19,7 +20,7 @@ void BFS(int G[][7], int size , int node ) {
 
 		for (int v = 1; v < size; v++) {
 
-			if (G[u][v] == 1 and visited[v] == 0) {
+			if (G[v][u] == 1 and visited[v] == 0) {
 
 				cout << v << " ";
 				visited[v] = 1;
@@ -30,6 +31,39 @@ void BFS(int G[][7], int size , int node ) {
 
 	cout << endl;
 }
+
+void DFS(int G[][7], int size , int node ) {
+
+	cout << "Depth First Traversal of Graph " << "using source " << node << " is: ";
+
+	int visited[size] = {0};
+
+	//cout << node << " ";
+	visited[node] = 1;
+	push(node);
+
+	while (!stkEmpty()) {
+
+		int u = pop();
+		cout << u << " ";
+
+		for (int v = 1; v < size ; v++) {
+
+			if (G[u][v] == 1 and visited[v] == 0) {
+
+				visited[v] = 1;
+				push(v);
+			}
+		}
+
+	}
+
+	cout << endl;
+
+}
+
+
+
 
 int main() {
 
@@ -43,7 +77,8 @@ int main() {
 		{0, 0, 0, 0, 1, 0, 0}
 	};
 
-	BFS(G, 7, 6);
+	BFS(G, 7, 5);
+	DFS(G, 7, 4);
 
 	return 0;
 }
