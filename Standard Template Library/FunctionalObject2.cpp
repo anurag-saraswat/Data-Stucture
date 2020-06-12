604d595061dc8fce8bdc697c5a6a5b98ed1ff23a
anurag-saraswat <saraswatanurag14@gmail.com>
2020-06-12 21:32:36 +0530

    Functional object in Priority Queue

---
+++ Standard Template Library/FunctionalObject2.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <cstring>
+#include <queue>
+using namespace std;
+
+
+class Person {
+
+public:
+
+	string name;
+	int age;
+
+	Person(string n, int a) {
+		name = n;
+		age = a;
+	}
+
+};
+
+class personCompare {
+
+public:
+
+	bool operator ()(Person a, Person b) {
+
+		return a.age > b.age;
+
+	}
+};
+
+int main() {
+
+	string name;
+	int age, n;
+
+	cin >> n;
+
+	priority_queue<Person, vector<Person>, personCompare> pq;
+
+
+	for (int i = 0; i < n; i++ ) {
+
+		cin >> name >> age;
+		Person p(name, age);
+
+		//cout << name << " " << age << endl;
+
+		pq.push(p);
+	}
+
+	cout << "Person in ascending order of age : " << endl;
+
+	while (!pq.empty()) {
+
+		Person per = pq.top();
+		cout << per.name << " " << per.age << endl;
+		pq.pop();
+
+	}
+
+	return 0;
+}
