750f04624f5ef2af1fed70891c51f949e2d2e51e
anurag-saraswat <saraswatanurag14@gmail.com>
2020-06-17 21:49:43 +0530

    SET Unique Permutation

---
+++ Standard Template Library/SET_Unique_Permutation.cpp
@@ -0,0 +1,41 @@
+#include "iostream"
+#include <set>
+#include <string>
+
+using namespace std;
+
+;
+
+
+void permute(string a, int i , set<string> &s ) {
+
+	if (a[i] == '\0') {
+		s.insert(a);
+		return;
+	}
+
+	//recursive case
+
+	for (int j = i; a[j] != '\0'; j++) {
+
+		swap(a[i], a[j]);
+		permute(a, i + 1, s);
+		swap(a[i], a[j]);
+
+	}
+}
+
+
+int main() {
+
+	set<string> s;
+	string a = "abb";
+
+	permute(a, 0, s);
+
+	for (auto it = s.begin(); it != s.end(); it++) {
+		cout << *it << endl;
+	}
+
+	return 0;
+}
