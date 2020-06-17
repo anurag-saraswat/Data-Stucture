a1aafa6f49092ccbe5885ad3c05e2faa53468e8d
anurag-saraswat <saraswatanurag14@gmail.com>
2020-06-17 18:24:28 +0530

    STL Set added

---
+++ Standard Template Library/SET.cpp
@@ -0,0 +1,43 @@
+// Collection of unique elements.
+// Underlying data structure Red Black Tree
+
+
+#include "iostream"
+#include<set>
+
+using namespace std;
+
+
+int main() {
+
+	int arr[] = {10, 20, 11, 30, 22, 40, 33, 10, 11};
+
+	int size = sizeof(arr) / sizeof(int) ;
+
+	set<int> s;
+
+	for (int i = 0; i < size; i++) {
+
+		s.insert(arr[i]);
+
+	}
+
+	//Delete Element
+
+	s.erase(22);
+
+	auto it = s.find(30);
+	s.erase(it);
+
+	// Print all element
+
+	for (set<int>::iterator it = s.begin(); it != s.end(); it++) {
+
+		cout << *it << ",";
+
+	}
+
+	cout << endl;
+
+	return 0;
+}
